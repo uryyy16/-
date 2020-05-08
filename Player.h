f48750f1ba42f0031ab550674d2b50293872128b
uryyy16 <59140659+uryyy16@users.noreply.github.com>
2020-05-08 22:59:39 +0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -3,6 +3,11 @@ class Player{
     char name[50];
     int score;
    public:
-    void eliminateRow();//判断是否能消除
-    void addRow();//给对方加行数
+    void kbhit();//检测键盘点击
+    void getCmd();//接受指令
+    void execCmd(Cmd cmd);//判断执行哪种指令
+    void moveRotate();//执行旋转指令
+    void moveLeft();//执行左移指令
+    void moveRight();//执行右移指令
+    void moveDown();//执行快速下降指令
 };
