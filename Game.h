bc96b5e9bdd33e3063c227c98f3595d7c5730001
uryyy16 <59140659+uryyy16@users.noreply.github.com>
2020-05-08 22:57:50 +0800

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,5 +1,12 @@
 class Game{
   public:
-    void Gameover();//判断再来一局还是退出界面
-    void Gamestart();//开始游戏
+    void initDatasPerRound();//每大局游戏初始化
+    void drawMap();//绘制地图
+    bool checkPut();//判断执行指令时是否越界
+    void execClear();//消行
+    void addRaw();//给对方加行
+    bool checkOver();//判断游戏是否结束
+    void recordSquareNow();//记录当前地图v
+    void winner();//判断赢家 
+    void setGameLevel();//判断游戏等级
 };
